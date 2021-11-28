9e24bc0ce808f56fd2eafd0850ce87dc971c2b10
Alejandro Vozella <alejandrovozella@gmail.com>
2021-11-28 10:54:50 -0300

    Funcion para Carga de Nombre de Jugadores

---
+++ app/scripts.js
@@ -0,0 +1,5 @@
+var nombre1 = prompt("Ingrese el nombre del Jugador 1","Jugador 1");
+var nombre2 = prompt("Ingrese el nombre del Jugador 2","Jugador 2");
+
+document.getElementById('jugador1').textContent = nombre1;
+document.getElementById('jugador2').textContent = nombre2;
